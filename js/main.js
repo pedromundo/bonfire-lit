9a55ed3964096f896d27bda137e822074ca2dc65
Pedro Raimundo <pedrooraimundo@gmail.com>
2021-06-08 22:14:07 +0200

    Photoshop original text out of the base image. Remove extraneous <p>. Update script to also draw text to the canvas.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ function doWork() {
     var myImg = new Image();
     myImg.onload = function () {
         context.drawImage(myImg, 0, 0, 1270, 720);
+        context.font = "72px OptimusPrinceps";
+        context.fillStyle = "#f8c35b";
+        context.textAlign = "center";
+        context.fillText("FOLHA DE PONTO PREENCHIDA", canvas.width / 2, (canvas.height / 2) + 20);
     };
     myImg.src = 'img/bonfire_lit.jpg';
 }
